152faf5c03cb1932f815b7b9c1760e58d73ca5d2
Klaus Ramm <klaus.ramm@tu-dresden.de>
2020-02-06 11:13:44 +0100

    Fix favicon content copy on build

--- src/assets/favicons/favicons.js
+++ src/assets/favicons/favicons.js
@@ -1,7 +1,7 @@
 const faviconsContext = require.context(
     '!!file-loader?name=[name].[ext]!.',
     true,
-    /\.(svg|png|ico|xml|json)$/
+    /\.(svg|png|ico|xml|json|webmanifest)$/
 );
 
 faviconsContext.keys().forEach(faviconsContext);
